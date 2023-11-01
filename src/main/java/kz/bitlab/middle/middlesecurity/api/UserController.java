6dc52ed5eaec1dc2baa5b9646375c0cba5a81496
ilyas <zhuanyshev.ilyas@gmail.com>
2023-11-01 20:37:13 +0600

    SUPER CODE

--- src/main/java/kz/bitlab/middle/middlesecurity/api/UserController.java
+++ src/main/java/kz/bitlab/middle/middlesecurity/api/UserController.java
@@ -5,14 +5,12 @@ import kz.bitlab.middle.middlesecurity.dto.UserChangePasswordDto;
 import kz.bitlab.middle.middlesecurity.dto.UserCreateDto;
 import kz.bitlab.middle.middlesecurity.dto.UserSignInDto;
 import kz.bitlab.middle.middlesecurity.service.UserService;
+import kz.bitlab.middle.middlesecurity.utils.UserUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -41,5 +39,21 @@ public class UserController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error on changing password");
         }
+    }
+
+    @PostMapping(value = "/update-own-password/{ownPassword}")
+    public ResponseEntity<?> changeOwnPassword(@PathVariable(name = "ownPassword") String ownPassword){
+        try{
+
+            String currentUser = UserUtils.getCurrentUserName();
+            if (currentUser != null) {
+                userService.changePassword(currentUser, ownPassword);
+                return ResponseEntity.ok("Password changes successfully");
+            }
+
+        }catch (RuntimeException e){
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Error on changing password");
     }
 }
